5fe1379fba99f719bff05eb85b9edec26203e698
Shanmuga-raj <rajmc222@gmail.com>
2022-02-18 13:48:59 +0530

    Created an API for Custom User Profile.

--- api/views.py
+++ api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.filters import SearchFilter
+from rest_framework.viewsets import ModelViewSet
+from .models import UserProfile
+from .permissions import UpdateOwnProfile
+from .serializers import UserProfileSerializer
+
 
 # Create your views here.
+class UserProfileViewSet(ModelViewSet):
+
+	serializer_class = UserProfileSerializer
+	queryset = UserProfile.objects.all()
+	permission_classes = (UpdateOwnProfile, )
+	filter_backends = (SearchFilter, )
+	search_fields = ('id', 'email', 'first_name')
